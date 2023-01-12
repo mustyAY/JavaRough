a6870fde736ba867f1a518eb50614be430d6e5db
Mustapha <mustyyahya.1580@yahoo.com>
2023-01-12 15:07:13 +0100

    first commit

---
+++ myscript.js
@@ -0,0 +1,31 @@
+//logical and
+
+const a = 3;
+const b = -2;
+
+console.log(a>0 && b>0);
+
+//logical or
+
+const c = 3;
+const d = -2;
+
+console.log(c > 0 || d > 0);
+
+//logical not
+const e = 3;
+const f = -2;
+console.log(!(e > 0 || f > 0))
+
+//data types
+//Numbers, strings, booleans:
+let length = 60.60;
+let color = "purple";
+let p = false;
+console.log(color)
+
+//object(name-value pairs), array object
+const person = {firstname: "Mustapha", LastName: "Yahya"};
+const foods = ["Bread", "Yam", "Rice"];
+console.log(person)
+console.log(foods)
